9f019ae8057fb59bc6e5e56f5f4cb61dda8de74c
Esmé Cowles <escowles@ucsd.edu>
2014-11-16 22:12:26 -0500

    Looking up new beers in Untappd

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 cellar - a simple app to track beers in my cellar
+
+environment variables:
+* UNTAPPD_CLIENT_ID - Untappd Client ID
+* UNTAPPD_CLIENT_SECRET - Untappd Client Secret
+* CELLAR_DATABASE_PASSWORD - Database password
+* SECRET_KEY_BASE - Rails Secret Key Base
--- app/helpers/beers_helper.rb
+++ app/helpers/beers_helper.rb
@@ -1,2 +1,27 @@
+require 'rest-client'
+require 'json'
+require 'cgi'
+
 module BeersHelper
+  def lookup( q )
+    options = Array.new
+    client = ENV['UNTAPPD_CLIENT_ID']
+    secret = ENV['UNTAPPD_CLIENT_SECRET']
+    url = "http://api.untappd.com/v4/search/beer?client_id=#{client}&client_secret=#{secret}&q=#{CGI.escape(q)}"
+    json_txt = RestClient.get(url)
+    json_obj = JSON.parse(json_txt)
+    beers = json_obj["response"]["beers"]["items"]
+    beers.each do |beer|
+      brewery = beer['brewery']
+      location = brewery['location']
+      loc = brewery['country_name']
+      loc += "--#{location['brewery_state']}" if loc == 'United States'
+      loc += "--#{location['brewery_city']}"
+      option = { id: beer["beer"]["bid"], name: beer["beer"]["beer_name"],
+                 brewery: brewery['brewery_name'], location: loc,
+                 style: beer["beer"]["beer_style"] }
+      options << option
+    end
+    options
+  end
 end
